1894533231d1fa28fcd31fb6ee5f434e26b5dd87
Martin SangDo <martin.sangdo@gmail.com>
2018-06-22 15:32:47 +0700

    - load more price

--- js/screen/market/index.js
+++ js/screen/market/index.js
@@ -25,13 +25,14 @@ class Market extends BaseScreen {
 				data_list: [],
 				loading_indicator_state: true,
 				isShowMore: false,
-				jwt: ''
+				jwt: '',
+				key_list: {}		//to make sure there is no duplicate coin in list
 			};
 		}
 		//
 		componentDidMount() {
 			//get top chart
-			this._get_price();
+			this._get_price(0);
 		}
 		//
 		_keyExtractor = (item) => item.index;
@@ -46,14 +47,18 @@ class Market extends BaseScreen {
 		);
 		//get latest price
 		_get_price = () => {
-			var url = API_URI.GET_CURRENT_PRICE + '&offset=' + this.state.offset;
+			this.setState({loading_indicator_state: true}, () => {
+				var url = API_URI.GET_CURRENT_PRICE + '&start=' + this.state.offset;
+				// Utils.dlog(url);
 				RequestData.sentGetRequest(url,
 					(detail, error) => {
 					if (detail != null && detail.data != null){
 						var list = detail.data;
 						var me = this;
+						var len = 0;
 						Object.keys(list).forEach(function(id) {
 							var coin_info = list[id];
+							if (!me.state.key_list[id] || me.state.key_list[id]==null){
 								me.state.data_list.push({
 									index: coin_info.id,
 									name: coin_info.name,
@@ -61,12 +66,23 @@ class Market extends BaseScreen {
 									price: coin_info.quotes['USD']['price'],
 									change: coin_info.quotes['USD']['percent_change_1h']
 								});
+								me.state.key_list[id] = true;
+							}
+							len++;
 						});
+						if (len < C_Const.PAGE_LEN){
+							//no more
+							this.setState({isShowMore: false});
+						} else {
+							this.setState({isShowMore: true});  //maybe have more
+						}
 					} else {
 							// Utils.xlog('error', error);
+							this.setState({isShowMore: false});
 					}
 					this.setState({loading_indicator_state: false});
 				});
+			});
 		};
 		//
 		_refresh_list = () => {
@@ -74,6 +90,14 @@ class Market extends BaseScreen {
 		};
 		//
 		_load_more = () => {
+			if (!this.state.loading_indicator_state && this.state.isShowMore){
+				this.setState({offset: this.state.offset + C_Const.PAGE_LEN}, () => {
+					this._get_price();
+				});
+			}
+		};
+		//
+		_open_search = () => {
 
 		};
 		//==========
@@ -89,10 +113,15 @@ class Market extends BaseScreen {
 									<Text style={common_styles.bold}>Latest price</Text>
 								</Body>
 								<Right style={[common_styles.headerRight, {flex:0.15}]}>
+									<Button
+										transparent
+										onPress={() => this._open_search()}
+									>
+										<Icon name="search" style={styles.header_icon}/>
+									</Button>
 								</Right>
 							</Header>
 							{/* END header */}
-							<Content>
 							<Spinner visible={this.state.loading_indicator_state} textStyle={common_styles.whiteColor} />
 							<View style={[styles.tbl_header]}>
 								<Text style={styles.td_item}>Name</Text>
@@ -100,8 +129,8 @@ class Market extends BaseScreen {
 								<Text style={styles.td_item}>Price (USD)</Text>
 								<Text style={styles.td_item}>Change 24h</Text>
 							</View>
-
-							<FlatList
+							<View style={{flex:1}}>
+								<FlatList
 											data={this.state.data_list}
 											renderItem={this._renderItem}
 											refreshing={false}
@@ -109,8 +138,9 @@ class Market extends BaseScreen {
 											onEndReachedThreshold={0.5}
 											keyExtractor={this._keyExtractor}
 											onEndReached={({ distanceFromEnd }) => this._load_more()}
+											initialNumToRender={20}
 										/>
-							</Content>
+							</View>
 						</Container>
 				);
 		}
